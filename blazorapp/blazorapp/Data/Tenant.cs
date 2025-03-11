fde80207546d8e48a9135a42316b6aa021fb97dc
Bazil Inayat <bazilinayat1@gmail.com>
2025-03-11 19:29:21 +0530

    Started updating the code and making a new blazor app

---
+++ blazorapp/blazorapp/Data/Tenant.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace blazorapp.Data
+{
+    /// <summary>
+    /// Class to represent the Tenant table in DB
+    /// </summary>
+    public class Tenant
+    {
+        /// <summary>
+        /// PK of the table
+        /// </summary>
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the tenant
+        /// This will be the entity name, in our case the restaurant name
+        /// </summary>
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This will be the name of the owner of the restaurant
+        /// </summary>
+        [Required]
+        [StringLength(100)]
+        public string OwnerName { get; set; }
+
+        /// <summary>
+        /// Different branches under this restaurant
+        /// </summary>
+        // Navigation properties
+        public virtual ICollection<Branch> Branches { get; set; }
+    }
+}
